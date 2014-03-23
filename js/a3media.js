c6c8cd15a4801bcf25a0af30a87d3d707b71417b
Alejandro Del Rincón <alexsp92@gmail.com>
2014-03-23 21:54:35 +0100

    Terminado el diseño para móviles de la interfaz principal

--- js/a3media.js
+++ js/a3media.js
@@ -1,16 +1,29 @@
 var verCanales = function () {
     $("#series").hide();
     $("#favoritos").hide();
+    $("#directos").hide();
+    $("#botonDirectos").show();
     $("#canales").show("fast");
 };
 
 var verSeries = function () {
     $("#canales").hide();
     $("#favoritos").hide();
+    $("#directos").hide();
+    $("#botonDirectos").show();
     $("#series").show("fast");
 };
 var verFavoritos = function () {
     $("#canales").hide();
     $("#series").hide();
+    $("#directos").hide();
+    $("#botonDirectos").show();
     $("#favoritos").show("fast");
+};
+var verDirectos = function () {
+    $("#canales").hide();
+    $("#series").hide();
+    $("#favoritos").hide();
+    $("#botonDirectos").hide();
+    $("#directos").show("fast");
 };
