0b1d579ab6fdffe6f7ba41408c31e5f104b488b5
Alejandro Del Rincón <alexsp92@gmail.com>
2014-03-25 23:12:23 +0100

    Insertamos un header

--- js/a3media.js
+++ js/a3media.js
@@ -27,15 +27,36 @@ var volver = function () {
     this.verCanales();
 };
 var programas = '[{ "nombre" : "La que se avecina", "favorito" : false, "emision" : "ahora"},{ "nombre" : "Salvados", "favorito" : false, "emision" : "mañana"}]';
+var header = function (posicion, nombre) {
+    var header = '<h3 class="topcoat-list__header">' + nombre + '</h3>';
+    insertarCadena(posicion, header);
+};
+
+function insertarCadena(element, html) {
+    var frag = document.createDocumentFragment(),
+        tmp = document.createElement('body'),
+        child;
+    tmp.innerHTML = html;
+    // Append elements in a loop to a DocumentFragment, so that the browser does
+    // not re-render the document for each node
+    while (child = tmp.firstChild) {
+        frag.appendChild(child);
+    }
+    element.appendChild(frag); // Now, append all elements at once
+    frag = tmp = null;
+}
 var cargar = function () {
+    var caca = '<h3 class="topcoat-list__header">Antena 3</h3>';
     var divCanales = document.getElementById("canales");
     var leido = JSON.parse(programas);
-    console.log(divCanales);
+
     console.log(leido);
     for (i = 0; i < leido.length; i++) {
-        console.log(1); console.log(leido[i]);
-        var textoNuevo = "<p> " + leido[i].favorito + " </p>";
-        console.log(textoNuevo);
-        divCanales.appendChild(document.createTextNode(textoNuevo));
+        console.log(1);
+        console.log(leido[i]);
+        header(divCanales, 'caca');
+        console.log(divCanales);
+        //        insertarCadena(divCanales, caca);
+        console.log(divCanales);
     }
 };
